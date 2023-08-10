74f585cc3d9f55588ba6278a2bdedf550977f6c6
Nidhish Srivastava <nidhish0430@gmail.com>
2023-08-11 00:22:02 +0530

    ..

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,8 @@
 import FetchUsers from "@/components/FetchUsers";
-import { Suspense } from "react";
-import Loading from "./loading";
-
 export default function Home() {
+  
+  //* NEW THING EXPLORED,if i make the component async and put a loading file in the directory,it automatically shows the loading state
+  //* No need to use Suspense with fallback as loading state or using next-dynamic(mixture of lazy loading with Suspense)
   return (
     <FetchUsers/>
   );
